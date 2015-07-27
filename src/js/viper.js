22ed8665cbb8e1dcabce25b0b50c740112a20396
Andrew Rose <andrew@roseconsulting.net>
2015-07-27 09:29:07 -0700

    Rebuilt viper.js to use JSON and functions to clean up code base

--- src/js/viper.js
+++ src/js/viper.js
@@ -1,49 +1,43 @@
-//read cookie and creates readable, individual cookie key:value pairs
-(function(){
-    var cookies;
 
-    function readCookie(name,c,C,i){
-        if(cookies){ return cookies[name]; }
-
-        c = document.cookie.split('; ');
-        cookies = {};
-
-        for(i=c.length-1; i>=0; i--){
-           C = c[i].split('=');
-           cookies[C[0]] = C[1];
-        }
-
-        return cookies[name];
-    }
-
-    window.readCookie = readCookie; // or expose it however you want
-})();
-
-//Global Variables
-var vipProfile = "", //Tealium Profile name
-    vipEnv = "prod",  //Tealium version - defaults to Prod and is changed below
-    vipCookie = readCookie("viper");
-
-//Checks query string and cookie.  Manipulates cookie and sets environment as required
-if (location.search.indexOf("viper=dev") && (!vipCookie)){
-	vipEnv = "dev";
-	document.cookie="viper=dev;";
-} else if (vipCookie === "dev"){
-	vipEnv = "dev";
-}
-if (location.search.indexOf("viper=prod") && (vipCookie === "dev" || vipCookie === "" || (!vipCookie))){
-	vipEnv = "prod";
-	document.cookie="viper=prod;";
-} else if (vipCookie === "prod"){
-	vipEnv = "prod";
-}
-
-//Adds "utag_data" if none exists and attaches script tag to page just before </body> tag to fire utag.js
-window.onload = function() {  
-    if (!utag_data){var utag_data = {};};
-    div = document.createElement("div"); div.setAttribute('id', 'viper'); div.style.visibility='hidden'; div.style.display = 'none';
-    document.body.appendChild(div);
-	a = '//tags.tiqcdn.com/utag/carb/'+ vipProfile + '/' + vipEnv + '/utag.js';
-	b = document.createElement("script"); b.src = a; b.type = 'text/javascript'; b.async = true;
-	document.getElementById('viper').appendChild(b);
-}
+var viper = {
+    setCookie: function(cname, cvalue, days) {
+                var d = new Date();
+                d.setTime(d.getTime() + (days*86400000));
+                var expires = "expires="+d.toUTCString();
+                document.cookie = cname + "=" + cvalue + "; " + expires;
+                },
+    getCookie: function(cname){
+                var name = cname + "=";
+                var ca = document.cookie.split(';');
+                for(var i=0; i<ca.length; i++) {
+                    var c = ca[i];
+                    while (c.charAt(0)==' ') c = c.substring(1);
+                    if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+                }
+                return "";
+                },
+    getQP: function(parameter){
+                var query = window.location.search.substring(1);
+                var vars = query.split("&");
+                for (var i=0;i<vars.length;i++) {
+                    var pair = vars[i].split("=");
+                    if(pair[0] == parameter){return pair[1];}
+                }
+                return(false);
+                },
+    environment: "",
+    application: "",
+    launch: function(){
+                if (!utag_data) {
+                    var utag_data = {};
+                }
+                this.getCookie("viper")?this.environment = this.getCookie("viper"):this.environment;
+                this.getQP("viper")?this.environment=this.getQP("viper"):this.environment;
+                this.setCookie("viper",this.environment);
+                div = document.createElement("div"); div.setAttribute('id', 'viper'); div.style.visibility='hidden';
+                div.style.display = 'none'; document.body.appendChild(div);
+                a = '//tags.tiqcdn.com/utag/carb/'+ this.application + '/' + this.environment + '/utag.js';
+                b = document.createElement("script"); b.src = a; b.type = 'text/javascript'; b.async = true;
+                document.getElementById('viper').appendChild(b);
+                }
+};
