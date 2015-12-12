20a598c55488ab1c8fa93c26f596f5d161e578a5
Andrew Rose <andrew@roseconsulting.net>
2015-12-11 17:53:57 -0800

    Added check for empty variable in Optimizely code

--- src/js/viper.js
+++ src/js/viper.js
@@ -3,28 +3,27 @@
 var utag_data = utag_data || {};
 
 //Optimizely tracking
-var optDomain = document.domain.toLowerCase(),
-    optID="";
-switch(optDomain) {
-    case "www.carbonite.com":
-        optID = "148582399";
-        break
-    case "www.carbonitestage.com":
-        optID = "538320036";
-        break
-    case "www.carbonitedev.com":
-        optID = "137050961";
-        break
-}
-if (optID) {
-    (function () {
+(function () {
+    var optDomain = document.domain.toLowerCase(),
+        optID="";
+    switch(optDomain) {
+        case "www.carbonite.com":
+            optID = "148582399";
+            break
+        case "www.carbonitestage.com":
+            optID = "538320036";
+            break
+        case "www.carbonitedev.com":
+            optID = "137050961";
+            break
+    }
+    if (optID) {
         var head = document.getElementsByTagName("head")[0];
         var opt = document.createElement("script");
         opt.src = '//cdn.optimizely.com/js/' + optID + '.js';
         head.insertBefore(opt, head.firstChild);
-    })();
-}
-
+    }
+})();
 
 
 //*********************************************----Viper Specific Code----**********************************************
